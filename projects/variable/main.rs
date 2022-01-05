08c514c6a8da8b1744cf2e3815b47a4f5f8f10a6
im <poson_hemp@yahoo.co.jp>
2022-01-05 12:59:05 +0900

    update/ calculation of rectangle

--- projects/variable/main.rs
+++ projects/variable/main.rs
@@ -1,5 +1,10 @@
 fn main() {
-  let length;
-  length = 3;
-  println!("辺の長さ: {}", length);
+  let min_length;
+  let max_length;
+  min_length = 3;
+  max_length = 5;
+
+  println!("長辺の長さ: {}", max_length);
+  println!("周長: {}", min_length * 2 + max_length * 2);
+  println!("面積: {}", min_length * max_length);
 }
