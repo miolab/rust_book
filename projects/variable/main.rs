ce298d5d43c25d9c483f1f6edf5c2fd484bed0ce
im <poson_hemp@yahoo.co.jp>
2022-01-05 13:02:37 +0900

    refactor/ variable

--- projects/variable/main.rs
+++ projects/variable/main.rs
@@ -1,9 +1,6 @@
 fn main() {
-  let min_length: i32;
-  let max_length: i32;
-
-  min_length = 3;
-  max_length = 5;
+  let min_length: i32 = 3;
+  let max_length: i32 = 5;
 
   println!("長辺の長さ: {}", max_length);
   println!("周長: {}", min_length * 2 + max_length * 2);
