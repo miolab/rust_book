6269f7f7dc491dc2a585d46a29a489beab05dd9d
im <poson_hemp@yahoo.co.jp>
2022-01-07 07:39:08 +0900

    prepare/ tuple

--- procon/pattern_match/src/main.rs
+++ procon/pattern_match/src/main.rs
@@ -1,3 +1,6 @@
 fn main() {
-    println!("Hello, world!");
+    let tuple: (i32, i32, i32) = (1, 2, 3);
+    println!("タプルの1番目要素は、{}", tuple.0);
+    println!("タプルの2番目要素は、{}", tuple.1);
+    println!("タプルの3番目要素は、{}", tuple.2);
 }
