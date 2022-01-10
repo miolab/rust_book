41863e4e579c7e77e42e7122b08c9e5e09e86d77
im <poson_hemp@yahoo.co.jp>
2022-01-10 14:13:24 +0900

    add/ for loop with pattern match

--- procon/for_and_ref/src/main.rs
+++ procon/for_and_ref/src/main.rs
@@ -4,8 +4,16 @@ fn main() {
         11,
         12,
     ];
-
     for p in &primes {
         println!("{}", p);
+    }
+
+    let elements: [(i32, f64); 3] = [
+        (6, 12.0),
+        (7, 14.0),
+        (8, 16.0),
+    ];
+    for &(number, weight) in &elements {
+        println!("{}: {:.1}", number, weight);
     }
 }
