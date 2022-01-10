e04cf872aaaef40b1ce9f98e95ee35353cad9b55
im <poson_hemp@yahoo.co.jp>
2022-01-10 14:30:59 +0900

    add/ for loop with ref

--- procon/for_and_ref/src/main.rs
+++ procon/for_and_ref/src/main.rs
@@ -16,4 +16,18 @@ fn main() {
     for &(number, weight) in &elements {
         println!("{}: {:.1}", number, weight);
     }
+
+    let elem = (10, 20.0);
+    let (ref num, ref wt) = elem;
+    assert_eq!(*num, 10);
+    assert_eq!(*wt, 20.0);
+
+    // ref を使った for loop 式
+    for (ref number, ref weight) in &elements {
+        println!("{}: {:.1}", number, weight)
+    }
+    // 上記式の ref は省略可能
+    for (number, weight) in &elements {
+        println!("{}: {:.1}", number, weight)
+    }
 }
