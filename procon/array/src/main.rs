bdf9c897bdd5350f754010f615859836f632d18b
im <poson_hemp@yahoo.co.jp>
2022-01-10 09:09:52 +0900

    add array example/ pattern match

--- procon/array/src/main.rs
+++ procon/array/src/main.rs
@@ -10,4 +10,10 @@ fn main() {
     assert_eq!(arr[1], 10_i32);
     assert_eq!(arr[2], 100_i32);
     println!("assertion OK.");
+
+    let [a, b, c] = arr;
+    assert_eq!(a, 1);
+    assert_eq!(b, 10);
+    assert_eq!(c, 100);
+    println!("pattern match OK.");
 }
