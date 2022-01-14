6e0f793c5d888d869a375f638b880b6c1d56d78b
im <poson_hemp@yahoo.co.jp>
2022-01-14 13:46:17 +0900

    prepare function for abc_preparation

--- procon/a_abc_preparation/src/main.rs
+++ procon/a_abc_preparation/src/main.rs
@@ -1,3 +1,15 @@
+use proconio::input;
+
 fn main() {
-    println!("Hello, world!");
+    input! {
+        arr: [i32; 4],
+    }
+
+    let mut answer = 100;
+    for &i in &arr {
+        if i < answer {
+            answer =i;
+        }
+    }
+    println!("{}", answer);
 }
