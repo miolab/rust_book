4176ef3b64f55b4116e53ca033d956ff3218bf7d
im <poson_hemp@yahoo.co.jp>
2022-01-17 09:49:31 +0900

    add/ break and continue in for loop

--- procon/loops_various/src/main.rs
+++ procon/loops_various/src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+    let arr = [11, 12, 13, 14, 15, 16];
+    for &i in &arr {
+        if i == 12 {
+            continue;
+        }
+        if i == 14 {
+            break;
+        }
+        println!("{}, ", i);
+    }
+    println!("done");
 }
