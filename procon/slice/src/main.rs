4b98ba9fb78bed6c905425641d6b4bc4103a7ca1
im <poson_hemp@yahoo.co.jp>
2022-01-18 08:47:42 +0900

    add/ slice type, type coercion

--- procon/slice/src/main.rs
+++ procon/slice/src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
-    println!("Hello, world!");
+    // スライス型 [T]
+    // - 配列 [T; N] の ; N を取り去った形
+    // - [T] 型の変数を宣言することはできない
+    let mut ref_slice: &[i32];
+
+    let array = [1, 2, 3];
+    ref_slice = &array;
+    println!("{:?}", ref_slice);
+
+    let vector = vec![4, 5, 6];
+    ref_slice = &vector;
+    println!("{:?}", ref_slice);
 }
