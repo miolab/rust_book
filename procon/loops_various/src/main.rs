28e539493fc98fdda118c63dcd22f7d11e9a183f
im <poson_hemp@yahoo.co.jp>
2022-01-17 13:47:25 +0900

    add/ loop

--- procon/loops_various/src/main.rs
+++ procon/loops_various/src/main.rs
@@ -1,4 +1,7 @@
+use proconio::input;
+
 fn main() {
+    // for 式（continue, break）
     let arr = [11, 12, 13, 14, 15, 16];
     for &i in &arr {
         if i == 12 {
@@ -11,10 +14,36 @@ fn main() {
     }
     println!("done");
 
+    // `..` 演算子
     for i in 21..25 {
         println!("{},", i)
     }
     for i in 31..=35 {
         println!("{},", i)
     }
+
+    // loop 式
+    loop {
+        input! {
+            x: i32,
+        }
+        if x > 0 {
+            println!("x 10: {},", x * 10);
+        } else {
+            break;
+        }
+    }
+
+    // loop 式（break で値を返す方法）
+    let val = loop {
+        input! {
+            x: i32,
+        }
+        if x > 0 {
+            println!("^2: {},", x * x);
+        } else {
+            break x;
+        }
+    };
+    println!("break with {}", val)
 }
