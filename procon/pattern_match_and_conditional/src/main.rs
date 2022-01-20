bb97394119829e850a2619291a5fd81a1f3b6ba4
im <poson_hemp@yahoo.co.jp>
2022-01-20 09:44:42 +0900

    add/ literal pattern

--- procon/pattern_match_and_conditional/src/main.rs
+++ procon/pattern_match_and_conditional/src/main.rs
@@ -1,3 +1,5 @@
+use proconio::input;
+
 fn main() {
     // 論駁不可能なパターン
     // let ref_slice: &[i32] = &[10, 15, 20];
@@ -9,5 +11,25 @@ fn main() {
         println!("{0}, {1}, {2}", x, y, z);
     } else {
         println!("Error! pattern match failed.");
+    }
+
+    // リテラルパターン
+    println!("タプルのベクタを入力:");
+    input! {
+        vector: [
+            (i32, i32); 3
+        ],
+    }
+    for &tuple in &vector {
+        if let (1, value) = tuple {
+            println!("{}", value);
+        } else if let (2, value) = tuple {
+            println!("{}", value * value);
+        } else if let (0, 0) = tuple {
+            println!("break");
+            break;
+        } else {
+            println!("?");
+        }
     }
 }
