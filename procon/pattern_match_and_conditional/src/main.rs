b80134e30571479a48aabedc538182af7fab95ed
im <poson_hemp@yahoo.co.jp>
2022-01-20 09:24:17 +0900

    add/ irrefutable pattern, refutable pattern

--- procon/pattern_match_and_conditional/src/main.rs
+++ procon/pattern_match_and_conditional/src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+    // 論駁不可能なパターン
+    // let ref_slice: &[i32] = &[10, 15, 20];
+
+    // 論駁可能なパターン
+    let ref_slice: &[i32] = &[10, 15];
+
+    if let [x, y, z] = *ref_slice {
+        println!("{0}, {1}, {2}", x, y, z);
+    } else {
+        println!("Error! pattern match failed.");
+    }
 }
