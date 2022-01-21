d1e0e1fe90834cf23c6b5ed117f4edbcb036f5d5
im <poson_hemp@yahoo.co.jp>
2022-01-21 13:52:21 +0900

    add/ several patterns

--- procon/pattern_match_and_conditional/src/main.rs
+++ procon/pattern_match_and_conditional/src/main.rs
@@ -13,14 +13,29 @@ fn main() {
         println!("Error! pattern match failed.");
     }
 
+    // 複数のパターン
+    let array = [
+        (1, 92),
+        (3, 91),
+        (95, 1),
+        (94, 2),
+    ];
+    let mut vector = Vec::new();
+    for tuple in &array {
+        if let (1, value) | (value, 2) = *tuple {
+            vector.push(value);
+        }
+    }
+    assert_eq!(vector, vec![92, 94]);
+
     // リテラルパターン
-    println!("タプルのベクタを入力:");
+    println!("タプルのベクタを入力 (3回):");
     input! {
-        vector: [
+        input_vector: [
             (i32, i32); 3
         ],
     }
-    for &tuple in &vector {
+    for &tuple in &input_vector {
         if let (1, value) = tuple {
             println!("{}", value);
         } else if let (2, value) = tuple {
