e4a6d5a353171476632b645e74c536417c717050
im <poson_hemp@yahoo.co.jp>
2022-01-28 07:59:25 +0900

    add/ String type, output

--- procon/string/src/main.rs
+++ procon/string/src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
-    println!("Hello, world!");
+    // 文字列の型
+    // String (Vec<T> に相当)
+    let s = String::new();
+    // str (スライス [T] に相当)
+    let slice: &str = &s;
+    println!("{}", slice); // カラ出力
+
+    // 文字列出力
+    let hello = "Hello";
+    let world = "world".to_string();
+    // - 文字列リテラルを使う必要がある。println!(hello); とかはエラーになる。
+    println!("{}, {}!", hello, world);
 }
