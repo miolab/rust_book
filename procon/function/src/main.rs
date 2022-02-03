d9b98a7332d8478120a386f0e0742d80c7979ee6
im <poson_hemp@yahoo.co.jp>
2022-02-04 08:45:36 +0900

    refactor/ prepare fn digits()

--- procon/function/src/main.rs
+++ procon/function/src/main.rs
@@ -1,12 +1,14 @@
 fn main() {
-    let vec = {
-        let mut v = Vec::new();
-        for i in 0..4 {
-            v.push(i);
-        }
-        v
-    };
+    let vec = digits();
     assert_eq!(vec, [
         0, 1, 2, 3,
     ]);
+}
+
+fn digits() -> Vec<i32> {
+    let mut v = Vec::new();
+    for i in 0..4 {
+        v.push(i);
+    }
+    v
 }
