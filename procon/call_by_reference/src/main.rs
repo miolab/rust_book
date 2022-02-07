557df9fa29897fd563f2272a052e58885daa36e3
im <poson_hemp@yahoo.co.jp>
2022-02-08 05:43:12 +0900

    add/ fn sum by call-by-reference

--- procon/call_by_reference/src/main.rs
+++ procon/call_by_reference/src/main.rs
@@ -1,3 +1,22 @@
 fn main() {
-    println!("Hello, world!");
+    // 参照渡し
+    let vector = vec![20, 80, 60, 40];
+    // let s = sum(vector); // 値渡しになりエラー
+    let s = sum(&vector);
+    assert_eq!(s, 200);
+    println!(
+        "{:?} の総和は {}",
+        vector,
+        s
+    )
+}
+
+// fn sum(v: Vec<i32>) -> i32 { // 値渡しになりエラー
+fn sum(v: &Vec<i32>) -> i32 {
+    let mut ret = 0;
+    // for &i in &v { // 値渡しになりエラー
+    for &i in v {
+        ret += i;
+    }
+    ret
 }
