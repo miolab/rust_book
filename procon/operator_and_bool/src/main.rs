493bd168eb9f75e53e07f08f857c5372c539b947
im <poson_hemp@yahoo.co.jp>
2022-02-15 09:22:13 +0900

    add/ basic operators

--- procon/operator_and_bool/src/main.rs
+++ procon/operator_and_bool/src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
-    println!("Hello, world!");
+    let actual_add = std::ops::Add::add(2, 3);
+    assert_eq!(actual_add, 2 + 3);
+    let actual_sub = std::ops::Sub::sub(20, 3);
+    assert_eq!(actual_sub, 20 - 3);
+    let actual_mul = std::ops::Mul::mul(20, 3);
+    assert_eq!(actual_mul, 20 * 3);
+    let actual_div = std::ops::Div::div(20, 3);
+    assert_eq!(actual_div, 20 / 3);
+    assert_eq!(actual_div, 6);
+    let actual_rem = std::ops::Rem::rem(20, 3);
+    assert_eq!(actual_rem, 20 % 3);
+    assert_eq!(actual_rem, 2);
 }
