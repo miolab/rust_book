c9b3e4c0939e00891ef847d551439fd12ca5bf91
im <poson_hemp@yahoo.co.jp>
2022-02-16 20:06:59 +0900

    add/ operator '=='

--- procon/operator_and_bool/src/main.rs
+++ procon/operator_and_bool/src/main.rs
@@ -1,3 +1,5 @@
+use proconio::input;
+
 fn main() {
     let actual_add = std::ops::Add::add(2, 3);
     assert_eq!(actual_add, 2 + 3);
@@ -11,4 +13,18 @@ fn main() {
     let actual_rem = std::ops::Rem::rem(20, 3);
     assert_eq!(actual_rem, 20 % 3);
     assert_eq!(actual_rem, 2);
+
+    input! {
+        x: i32,
+    }
+    is_int_five(x);
+}
+
+fn is_int_five(a: i32) {
+    if a == 5 {
+        println!("this is equal to 5!");
+    }
+    if PartialEq::eq(&a, &5) {
+        println!("this is equal to 5! (Re)");
+    }
 }
